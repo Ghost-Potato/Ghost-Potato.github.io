8fecc57ca075947cb305003b0729a77ba0ba9f66
jamiemacdonald <jamiemacdonald473@gmail.com>
2025-02-09 20:52:09 -0500

    finished lab 2 for JavaScript

--- js/lab-2.js
+++ js/lab-2.js
@@ -1,63 +1,85 @@
 /* Declare and initialize global variables
 		-------------------------------------------------- */
-        var pageBg = document.querySelector("html");
-        var sliders = document.getElementsByTagName("input");
-        var rgb = [0, 0, 0];
-        
-        /* Event handlers for range sliders
+var pageBg = document.querySelector("html");
+var sliders = document.getElementsByTagName("input");
+var rgb = [0, 0, 0];
+
+/* Event handlers for range sliders
                 -------------------------------------------------- */
-        for (var i = 0; i < sliders.length; i++) {
-          // Loop through the three range inputs and for each one, add an onchange event listener
-          sliders[i].onchange = function () {
-            // If an input range slider is moved, grab it’s id attribute value
-            var whichSlider = this.getAttribute("id");
-            // …also, grab the numerical value that it is set to
-            var sliderValue = this.value;
-            // Declare a new variable to hold the new RGB value that calls a function that updates the global rgb variable, passing in what slider was moved (whichSlider), and its value (sliderValue)
-            newRgb = changeRgb(whichSlider, sliderValue);
-            // Call a function that builds a new CSS background-color property (as a string), passing it the updated RGB array (newRgb)
-            var newCSS = writeCSS(newRgb);
-            // Directly change the background-color of the page using the new CSS rgb value
-            pageBg.style.backgroundColor = newCSS;
-          };
-        }
-        
-        /* Functions
+for (var i = 0; i < sliders.length; i++) {
+  // Loop through the three range inputs and for each one, add an onchange event listener
+  sliders[i].onchange = function () {
+    // If an input range slider is moved, grab it’s id attribute value
+    var whichSlider = this.getAttribute("id");
+    // …also, grab the numerical value that it is set to
+    var sliderValue = this.value;
+    // Declare a new variable to hold the new RGB value that calls a function that updates the global rgb variable, passing in what slider was moved (whichSlider), and its value (sliderValue)
+    newRgb = changeRgb(whichSlider, sliderValue);
+    // Call a function that builds a new CSS background-color property (as a string), passing it the updated RGB array (newRgb)
+    var newCSS = writeCSS(newRgb);
+    // Directly change the background-color of the page using the new CSS rgb value
+    pageBg.style.backgroundColor = newCSS;
+  };
+}
+
+/* Functions
                 -------------------------------------------------- */
-        // STEP 1: Write a function called changeRgb that accepts two
-        //parameters, channel and value
-        function changeRgb(channel, value) {
-          switch (value) {
-            case red:
-        
-            case green:
-            case blue:
-          }
-        }
-        // STEP 2: Build a switch based on the value of the channel
-        //parameter (red, green, or blue) (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch)
-        
-        // STEP 3: Inside each case, update the appropriate
-        // global rgb array element (0, 1, or 2)
-        
-        // STEP 4: Return the updated rgb array back to the event handler
-        
-        // STEP 5: Write a new function called writeCSS that accepts one parameter, the updated rgb array
-        function writeCSS(rgbArray) {}
-        /* STEP 6: Declare a new local variable called newColor that will contain the new string that will be used to update the CSS 
+// STEP 1: Write a function called changeRgb that accepts two
+//parameters, channel and value
+function changeRgb(channel, value) {
+  switch (channel) {
+    case "red":
+      rgb[0] = value; //updating the value based on the channel index in the rgb array. 0 for red, 1 for green, and 2 for blue. The value should be between 0 and 255.
+      break;
+    case "green":
+      rgb[1] = value;
+      break;
+    case "blue":
+      rgb[2] = value;
+    //no default, as it seems irrelevant for this scenario
+  }
+  return newRgb;
+}
+// STEP 2: Build a switch based on the value of the channel
+//parameter (red, green, or blue) (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch)
+
+// STEP 3: Inside each case, update the appropriate
+// global rgb array element [0, 1, 2]
+
+// STEP 4: Return the updated rgb array back to the event handler
+
+// STEP 5: Write a new function called writeCSS that accepts one parameter, the updated rgb array
+function writeCSS(newRgb) {
+  var newColor = "rgb(";
+  var i = 0; //using this as the initializer for the while loop
+  while (i < newRgb.length) {
+    //adding the newColor value to the newRgb array. Should loop through each element in the array to complete the rgb(value, value, value)
+    newColor += newRgb[i];
+    //adding a comma and space between each value
+    newColor += ", ";
+    //incrementing the value of i/the index of the newRgb array
+    i++;
+  }
+  //including all the elements except the last comma and space with slice
+  newColor = newColor.slice(0, -2);
+  //adding the )" to the end of the string
+  newColor += ")";
+  //returning the newColor to the event handler
+  return newColor;
+}
+/* STEP 6: Declare a new local variable called newColor that will contain the new string that will be used to update the CSS 
                    background-color property in the following format: rgb(0,0,0) - initialize it with the start of the string, 'rgb(' */
-        
-        // STEP 7: Create a while loop that iterates through the array passed into this function, called newRgb (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while)
-        
-        // STEP 8: For each element of the array, add to the string newColor, the red, green, and blue values, each followed by a comma
-        
-        // STEP 9: Slice off the last comma from the string contained by the variable, newColor - we don’t need it (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice)
-        
-        // STEP 10: Finish off the newColor string by adding the closing ')'
-        
-        // STEP 11: Return the string newColor back to the event handler
-        
-        // STEP 12: Enjoy the application in your browser!
-        
-        // This page inspired by https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Return_values
-        
+
+// STEP 7: Create a while loop that iterates through the array passed into this function, called newRgb (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while)
+
+// STEP 8: For each element of the array, add to the string newColor, the red, green, and blue values, each followed by a comma
+
+// STEP 9: Slice off the last comma from the string contained by the variable, newColor - we don’t need it (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice)
+
+// STEP 10: Finish off the newColor string by adding the closing ')'
+
+// STEP 11: Return the string newColor back to the event handler
+
+// STEP 12: Enjoy the application in your browser!
+
+// This page inspired by https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Return_values
